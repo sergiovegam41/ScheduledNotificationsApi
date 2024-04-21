e6add2c9615ffe3cba0b16a4bb127034c8d755a8
sergiovegam41 <59804620+sergiovegam41@users.noreply.github.com>
2024-04-21 11:01:06 -0500

    save

--- src/Controllers/NotificationsController.js
+++ src/Controllers/NotificationsController.js
@@ -23,7 +23,6 @@ class NotificationsController {
 
     static async sendNotifyManyByFilterV2(MongoClient, cities = ["649a034560043e9f434a94fe"], professions = ["64c553e73abc6c0ec50e1dc3"], title = "Hola! $[user_name];! bienvenido a Dservices ", body="Dservices te desea un feliz $[dayWeekName];!", role = "TECNICO", tipo="comun"){
 
-        
         const FIREBASE_TOKEN = (await MongoClient.collection(DBNames.Config).findOne({ name: "FIREBASE_TOKEN" })).value;
         const TokenWebhook = (await MongoClient.collection(DBNames.Config).findOne({ name: "TokenWebhook" })).value;
         const HostBotWhatsApp = (await MongoClient.collection(DBNames.Config).findOne({ name: "HostBotWhatsApp" })).value;
--- src/Jobs/ScheduledNotifications/ScheduledNotifications.js
+++ src/Jobs/ScheduledNotifications/ScheduledNotifications.js
@@ -23,19 +23,28 @@ class ScheduledNotifications {
             for (let element of scheduled_notifications ) {
                 
                 
-                // static async sendNotifyManyByFilterV2(MongoClient, cities = ["649a034560043e9f434a94fe"], professions = ["64c553e73abc6c0ec50e1dc3"], title = "Hola! $[user_name];! bienvenido a Dservices ", body="Dservices te desea un feliz $[dayWeekName];!", role = "TECNICO", tipo="comun"){
                 let AllCities = []
                 let departments = await MongoClient.collection(DBNames.departments).find({ countri_id: element.country_id}).toArray();
-                    
-                    
+                
+                
                 for (let departament of departments ) {
-
-                    let cities = await MongoClient.collection(DBNames.cities).find({ departament_id: departament._id.toString()}).toArray();
-                    AllCities.push(...cities);
+                    
+                    let cities = await MongoClient.collection(DBNames.municipalities).find({ departament_id: departament._id.toString()}).toArray();
+                    let cityIds = cities.map(city => city._id.toString()); // Transforma cada ciudad a su ID en formato string
+                    AllCities.push(...cityIds); 
                 }
-
-
-               await NotificationsController.sendNotifyManyByFilterV2(MongoClient, AllCities, element.profession_filter, element.title, element.description,element.title, element.role??"TECNICO",tipo="comun")
+                
+                
+                // static async sendNotifyManyByFilterV2(MongoClient, cities = ["649a034560043e9f434a94fe"], professions = ["64c553e73abc6c0ec50e1dc3"], title = "Hola! $[user_name];! bienvenido a Dservices ", body="Dservices te desea un feliz $[dayWeekName];!", role = "TECNICO", tipo="comun"){
+                    // await NotificationsController.sendNotifyManyByFilterV2(MongoClient,req,[session.location.municipality_id],[form.professions_id],`Nuevo servicio ${parseInt(priceService)>0?"~ "+cantidadFormateada: ""}`, `Hola $[user_name];, tenemos un Nuevo servicio disponible para ti ${main_address.value.district != ""?"en "+main_address.value.district:""}`, "TECNICO","new_services")
+                    console.log(AllCities);
+                    console.log(element.profession_filter);
+                    console.log(element.title);
+                    console.log(element.description);
+                    console.log(element.title);
+                    console.log("comun");
+               
+                    await NotificationsController.sendNotifyManyByFilterV2(MongoClient, AllCities, element.profession_filter, element.title, element.description, element.role,"comun")
             //    await NotificationsController.sendNotifyManyByFilter(MongoClient,element.title,element.description,"comun",element)
                 
             };
